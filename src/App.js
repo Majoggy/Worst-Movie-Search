a6303a02b9b5e77c92cd81a6a00a1eb4b0271a42
Christian Baker <cbaker87@gmail.com>
2023-06-23 00:02:58 +0100

    Cleaned up formatting and logic

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable camelcase */
+
 import React from 'react'
 import { getActor, getFilmography, getMovie } from './lib/api'
 import { sortByVotes } from './lib/helpers'
@@ -9,118 +11,144 @@ function App() {
   const [searchFail, setSearchFail] = React.useState(false)
   const [reload, setReload] = React.useState(false)
   const [darkMode, setDarkMode] = React.useState(false)
-  let actorId = null
 
   const setSearch = (e) => {
     const currentSearch = e.target.value
     setSearchTerm(currentSearch)
-    console.log(currentSearch)
   }
 
   const handleSearch = async (e) => {
     setReload(false)
     e.preventDefault()
+
     try {
-      const actorData = await getActor(searchTerm)
-      if (actorData.data.total_results === 0 || actorData.data.results[0].known_for_department !== 'Acting' || actorData.data.results[0].id === 2570957) {
-        baconTime()
-      } else {
-        setSearchFail(false)
-        actorId = actorData.data.results[0].id
-        setActor(actorData.data.results[0].name)
-      }      
-      filmTime(actorId)
-      setReload(true)
+      const response = await getActor(searchTerm)
+
+      if (response === null) {
+        return failSearch()
+      }
+
+      const { id, name } = response.results[0]
+
+      setSearchFail(false)
+      getFilms(id)
+      setActor(name)
     } catch (err) {
       console.log(err)
     }
   }
 
-  const filmTime = async (actorId) => {
+  const failSearch = () => {
+    setSearchFail(true)
+    getFilms(4724)
+    setActor('Kevin Bacon')
+    setReload(true)
+  }
+
+  const getFilms = async (actorId) => {
     try {
       const response = await getFilmography(actorId)
-      if (sortByVotes(response.data.cast).filter(movie => movie.vote_count > 25).map(movie => movie).length === 0) {
-        console.log('Actor result, but no film data')
-        baconTime()
-        const response = await getFilmography(4724)
-        setData(sortByVotes(response.data.cast))
-      } else setData(sortByVotes(response.data.cast))
+
+      if (response === null) {
+        return failSearch()
+      }
+
+      setData(sortByVotes(response.cast))
     } catch (err) {
       console.log(err)
     }
-  }
-
-  const baconTime = () => {
-    console.log('Bacon time!')
-    setSearchFail(true)
-    actorId = 4724
-    setActor('Kevin Bacon')
   }
 
   const handleLink = async (e) => {
     try {
       const movieId = e.target.id
-      const response = await getMovie(movieId)
-      const imdbLink = `https://www.imdb.com/title/${response.data.imdb_id}/?ref_=fn_al_tt_2`
-      window.location.href = imdbLink
+      const {
+        data: { imdb_id },
+      } = await getMovie(movieId)
+      window.location.href = `https://www.imdb.com/title/${imdb_id}/?ref_=fn_al_tt_2`
     } catch (err) {
       console.log(err)
     }
   }
 
   return (
-    <section className={ darkMode ? 'body dark-mode' : 'body'}>
-      <section className='app-wrap'>
-        <h1 onClick={()=> {
-          if (darkMode === true) {
-            setDarkMode(false) 
-          } else setDarkMode(true)
-        }}>Worst Movie Search </h1>
+    <section className={darkMode ? 'body dark-mode' : 'body'}>
+      <section className="app-wrap">
+        <h1 onClick={darkMode ? setDarkMode(false) : setDarkMode(true)}>
+          Worst Movie Search{' '}
+        </h1>
         <p>Your favourite actors at their lowest ebb </p>
-        <form className='search-form'>
-          <input id='search-bar' className={darkMode ? 'dark-mode' : '' } type='text' onChange={setSearch} />
-          <button 
-            id='search-button' 
-            type='submit' 
-            onMouseUp={(e)=>{
+        <form className="search-form">
+          <input
+            id="search-bar"
+            className={darkMode ? 'dark-mode' : ''}
+            type="text"
+            onChange={setSearch}
+          />
+          <button
+            id="search-button"
+            type="submit"
+            onMouseUp={(e) => {
               e.target.style.backgroundColor = 'rgb(238, 238, 238)'
-            }} 
-            onMouseDown={(e)=>{
+            }}
+            onMouseDown={(e) => {
               e.target.style.backgroundColor = 'rgb(215, 215, 215)'
-            }} 
-            onClick={handleSearch}>Search</button>
+            }}
+            onClick={handleSearch}
+          >
+            Search
+          </button>
         </form>
-        {data &&
-      <>
-        {searchFail ? <h4 className={ reload ? 'reload' : ''}>I couldn&apos;t find that, so I found {actor} instead</h4> : <h4 className={ reload ? 'reload' : ''}> Results for {actor}</h4>}
-        <div className={ reload ? 'reload poster-wrap' : 'poster-wrap'}>
-          {data && data.filter(movie => movie.vote_count > 25).map(movie =>
-            <div key={movie.title}  className='result'>
-              <img 
-                width='200px' 
-                onClick={handleLink}
-                onMouseDown={(e) => {
-                  e.target.classList = 'darker poster'
-                }}
-                onMouseUp={(e => {
-                  e.target.classList = 'poster'
-                })} 
-                className={darkMode ? 'poster p-dark-mode' : 'poster'}
-                id={movie.id} 
-                src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} 
-                onError={(event) => event.target.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png'}/> 
+        {data && (
+          <>
+            {searchFail ? (
+              <h4 className={reload ? 'reload' : ''}>
+                I couldn&apos;t find that, so I found {actor} instead
+              </h4>
+            ) : (
+              <h4 className={reload ? 'reload' : ''}> Results for {actor}</h4>
+            )}
+            <div className={reload ? 'reload poster-wrap' : 'poster-wrap'}>
+              {data &&
+                data
+                  .filter((movie) => movie.vote_count > 25)
+                  .map((movie) => (
+                    <div key={movie.title} className="result">
+                      <img
+                        width="200px"
+                        onClick={handleLink}
+                        onMouseDown={(e) => {
+                          e.target.classList = 'darker poster'
+                        }}
+                        onMouseUp={(e) => {
+                          e.target.classList = 'poster'
+                        }}
+                        className={darkMode ? 'poster p-dark-mode' : 'poster'}
+                        id={movie.id}
+                        src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+                        onError={(event) =>
+                          (event.target.src =
+                            'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png')
+                        }
+                      />
+                    </div>
+                  ))
+                  .slice(0, 3)}
             </div>
-          ).slice(0, 3)}  
-        </div>
-        <div className={ reload ? 'reload results-wrap' : 'results-wrap'}>
-          {data && data.filter(movie => movie.vote_count > 25).map(movie =>
-            <div key={movie.title} className='result'>
-              <p className='title'>{movie.title}</p>
-              <p>({movie.release_date.substring(0,4)})</p>
+            <div className={reload ? 'reload results-wrap' : 'results-wrap'}>
+              {data &&
+                data
+                  .filter((movie) => movie.vote_count > 25)
+                  .map((movie) => (
+                    <div key={movie.title} className="result">
+                      <p className="title">{movie.title}</p>
+                      <p>({movie.release_date.substring(0, 4)})</p>
+                    </div>
+                  ))
+                  .slice(0, 3)}
             </div>
-          ).slice(0, 3)}  
-        </div>
-      </>}
+          </>
+        )}
       </section>
     </section>
   )
--- src/lib/api.js
+++ src/lib/api.js
@@ -1,14 +1,39 @@
 import axios from 'axios'
+import { sortByVotes } from './helpers'
 
 const baseUrl = 'https://api.themoviedb.org/3'
 const apiKey = process.env.REACT_APP_API_KEY
 
-export function getActor(actor) {
-  return axios.get(`${baseUrl}/search/person?api_key=${apiKey}&query=${actor}`)
+export async function getActor(actor) {
+  const { data } = await axios.get(
+    `${baseUrl}/search/person?api_key=${apiKey}&query=${actor}`
+  )
+
+  if (
+    data.total_results === 0 ||
+    data.results[0].known_for_department !== 'Acting' ||
+    data.results[0].id === 2570957
+  ) {
+    return null
+  }
+
+  return data
 }
 
-export function getFilmography(actorId) {
-  return axios.get(`${baseUrl}/person/${actorId}/movie_credits?api_key=${apiKey}`)
+export async function getFilmography(actorId) {
+  const { data } = await axios.get(
+    `${baseUrl}/person/${actorId}/movie_credits?api_key=${apiKey}`
+  )
+
+  if (
+    sortByVotes(data.cast)
+      .filter((movie) => movie.vote_count > 25)
+      .map((movie) => movie).length === 0
+  ) {
+    return null
+  }
+
+  return data
 }
 
 export function getMovie(movieId) {
